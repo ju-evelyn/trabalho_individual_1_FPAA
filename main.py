42b1109677748c93f9feb98fc03ec0dd10d13f9d
Júlia Evelyn <72927574+ju-evelyn@users.noreply.github.com>
2025-02-19 22:51:44 -0300

    refactor: atualizando nome das variáveis

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def karatsuba(num1, num2):
     n = max(len(str(num1)), len(str(num2)))
     m = n // 2
 
-    parte1num1, parte2num1 = divmod(num1, 10 ** m)
-    parte1num2, parte2num2 = divmod(num2, 10 ** m)
+    esq1, dir1 = divmod(num1, 10 ** m)
+    esq2, dir2 = divmod(num2, 10 ** m)
 
     # Recursividade (divisão)
-    rec1 = karatsuba(parte1num1, parte1num2)
-    rec2 = karatsuba(parte2num1, parte2num2)
-    rec3 = karatsuba((parte2num1 + parte1num1), (parte2num2 + parte1num2))
+    rec1 = karatsuba(esq1, esq2)
+    rec2 = karatsuba(dir1, dir2)
+    rec3 = karatsuba((dir1 + esq1), (dir2 + esq2))
 
     # Combinação (conquista)
     return (rec1 * 10**(2*m)) + ((rec3 - rec1 - rec2) * 10**m) + rec2
